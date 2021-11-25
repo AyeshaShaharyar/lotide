190bae5c14d3b5d62ebbdea449638232eacc06fb
AyeshaShaharyar <ayesha2598@gmail.com>
2021-11-25 02:15:07 +0000

    assertion test

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 // Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
 const assertEqual = function(actual, expected) {
   if (actual  === expected){
-    console.log(" ✅ ✅ ✅  Assertion Passed: " + [actual] + " === " + [expected]);
+    return(`✅ ✅ ✅  Assertion Passed: ${actual}  ===  ${expected}`);
   } else {
-    console.log(" 🛑 🛑 🛑  Assertion Failed: " + [actual] + " !== " + [expected]);
+    return(`🛑 🛑 🛑  Assertion Failed: ${actual}  !==  ${expected}`);
   };
 };
 
