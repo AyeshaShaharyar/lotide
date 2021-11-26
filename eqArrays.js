1918e577bc9fdc2545f71bd3dada07c81bedf476
AyeshaShaharyar <ayesha2598@gmail.com>
2021-11-26 00:26:39 +0000

    new function correct version

--- eqArrays.js
+++ eqArrays.js
@@ -8,23 +8,20 @@ const assertEqual = function(actual, expected) {
 
 
 const eqArrays = function(array1, array2){
-let values1;
-let  values2;
-{
-  for(let value of array1){
-     values1 = value;
-  }
-  for (let value of array2){
-    values2 = value;
-  }
-  if(values1 === values2){
-    return true ;
-  }else{
+  if(array1.length !== array2.length){
     return false;
   }
+  for (let i = 0; i < array1.length; i++){
+  if(array1[i] !== array2[i]){
+    return false;
+  };
 }
-}
+return true;
+};
+
+
+
 console.log(eqArrays([1,2,3], [1,2,3]));
-console.log(eqArrays([1,2,30], [1,2,3]));
-console.log(eqArrays([1,2,30], [1,2,30]));
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
+console.log(eqArrays([1,2,3], [1,2,3]));
+console.log(eqArrays([1,2,3,6], [1,2,3,6]));
+console.log(assertEqual(eqArrays([1, 20, 3], [1, 2, 3]), true)); // => should PASS
