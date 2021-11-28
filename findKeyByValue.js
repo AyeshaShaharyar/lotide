14f26172d15fe71c12069f93def2cd5cb231a175
AyeshaShaharyar <ayesha2598@gmail.com>
2021-11-28 17:16:33 +0000

    key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual  === expected){
+    return(`✅ ✅ ✅  Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    return(`🛑 🛑 🛑  Assertion Failed: ${actual}  !==  ${expected}`);
+  };
+};
+
+
+const findKeyByValue = function(obj, val){
+let keys = (Object.keys(obj));
+  for (let key of keys){
+    if(obj[key] === val){
+    return key;
+  }
+}
+
+};
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
