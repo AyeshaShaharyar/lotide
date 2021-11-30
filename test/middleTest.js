96d7ceb22989a1d1a6006a3100aa6a4fc047fcc0
AyeshaShaharyar <ayesha2598@gmail.com>
2021-11-30 03:24:22 +0000

    exporting all the functions in one main file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,22 @@
-const assertArraysEqual = require("../assertArraysEqual");
-const middle = require("../middle.js")
+const assert = require("chai").assert;
+const middle = require("../middle.js");
 
-console.log(middle([1,2]));
+// console.log(middle([1,2]));
 
-console.log(assertArraysEqual(middle([1,2,3]), [2])); // => should PASS
-console.log(assertArraysEqual(middle([1,2,3,4]), [2,3]));
+// console.log(assertArraysEqual(middle([1,2,3]), [2])); // => should PASS
+// console.log(assertArraysEqual(middle([1,2,3,4]), [2,3]));
+
+describe("return the middle of the array", () => {
+
+  it("return the middle of the array [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+
+  it("return the middle of the array [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+
+  it("return the empty array [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,9 @@
+const assert = require("chai").assert;
 const tail = require("../tail.js");
-const assertEqual = require("../assertEqual");
-// TEST CODE
-const result1 = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result1);
 
-//Function Tail not modifying the original array check:
+describe("tail of the array", () => {
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), (["Lighthouse", "Labs"]));
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3));
-
-//Assertion Check:
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2)); // ensure we get back two elements
-console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
-console.log(assertEqual(result[1], "Labs"));
+});
